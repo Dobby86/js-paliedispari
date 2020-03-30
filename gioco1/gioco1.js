8dda53734ec856fc6a1374d4fea73ce9ff548ee8
marco <dobermannpoker86@gmail.com>
2020-03-30 21:50:31 +0200

    primo esercizio working in progress

--- gioco1/gioco1.js
+++ gioco1/gioco1.js
@@ -4,30 +4,44 @@
 // tramite una funzione (creata da te) verifico se la parola inserita è palindroma o meno;
 // dò output a utente dicendogli che ho verificato;
 // qui sicuramente da cercare ci sarà il come revertare una parola
-var parola;
-var risultato;
-var parolaInversa;
-var isPalindroma;
+var parola= document.getElementById("chiedere");
+var mettiNome=parola.value;
 
-function isPalindroma(parola) {
+var stampa=document.getElementById("risultato");
+stampa.innerHTML=mettiNome;
+var bottone =document.getElementById("push");
 
-
-    var parolaInversa = parola.reverse;
-
-    if (parola==parolaInversa){
-        risultato="well";
-        console.log(risultato);
-    }else {
-        console.log();
-    }
-}
-
-// devi dirmi se la parola è palindroma
-
-var parolaPalindroma = isPalindroma("anna");
-
-if (parolaPalindroma == true) {
-console.log();
-} else {
-    console.log();
-}
+bottone.addEventListener("click",
+// var ghost = document.getElementById("dasparire");
+// ghost.style.color="initial";
+   function() {
+ var mettiNome = parola.value;
+   //stampo nome
+   var stampa = document.getElementById("risultato");
+    stampa.innerHTML = mettiNome;
+// var parolaInversa;
+// var isPalindroma;
+  }
+) ;
+// function isPalindroma(parola) {
+//
+//
+//     var parolaInversa = parola.reverse;
+//
+//     if (parola==parolaInversa){
+//         risultato="well";
+//         console.log(risultato);
+//     }else {
+//         console.log();
+//     }
+// }
+//
+// // devi dirmi se la parola è palindroma
+//
+// var parolaPalindroma = isPalindroma("anna");
+//
+// if (parolaPalindroma == true) {
+// console.log();
+// } else {
+//     console.log();
+// }
